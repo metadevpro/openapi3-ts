a115e373e5d6aabe263fecedf8e49e3e6e4ee991
Robin Tail <robin_tail@me.com>
2023-03-29 20:43:07 +0200

    Additional assetions for the OAS version of the builders.

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,10 +5,12 @@ describe('Top barrel', () => {
     it('OpenApiBuilder v. 3.0 is exported', () => {
         const sut = oas30.OpenApiBuilder.create();
         expect(sut).not.toBeNull;
+        expect(sut.rootDoc.openapi).toBe("3.0.0");
     });
     it('OpenApiBuilder v. 3.1 is exported', () => {
         const sut = oas31.OpenApiBuilder.create();
         expect(sut).not.toBeNull;
+        expect(sut.rootDoc.openapi).toBe("3.1.0");
     });
     it('Server is exported', () => {
         const sut = new Server('a', 'b');
