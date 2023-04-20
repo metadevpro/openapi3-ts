56d7df81f556f10797317bb2629c0419df57fbf0
koooge <koooooge@gmail.com>
2023-04-20 22:17:17 +0200

    chore: Build mjs and cjd with vite
    Signed-off-by: koooge <koooooge@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vitest/config';
 import TeamCityReporter from 'vitest-teamcity-reporter';
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'OpenApi3TS',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['yaml'],
+      output: {
+        globals: {
+          yaml: 'yaml',
+        },
+      },
+    },
+  },
     test: {
         environment: 'node',
         include: ['src/**/*.spec.ts'],
